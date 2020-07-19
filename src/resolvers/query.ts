797d63b6bbb1076d0957f5ebc4a273de5d5db9c0
mabvmex <mabvmex@gmail.com>
2020-07-19 17:07:23 -0500

    Se obtiene Path de temporadas, Se añade la definicion y obetenemos la fuente de datos.

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,8 +1,13 @@
 import { IResolvers } from 'graphql-tools';
+import { dataSources } from '../data/index';
 
 const query: IResolvers = {
     Query: {
-        
+        async seasonList(_: void, __: any, {dataSources}) {
+            return await dataSources.seasons.getSeasons().then(
+                (data: any) => data.MRData.SeasonTable.Seasons
+            );
+        }
     }
 };
 
