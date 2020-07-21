db291a70c0011c3c1dcedfd8ebe82ce7f9a2df5d
mabvmex <mabvmex@gmail.com>
2020-07-20 23:07:42 -0500

    Schema listaPilotos; Lista Todos; paginacon

--- src/data/data-drivers.ts
+++ src/data/data-drivers.ts
@@ -1,14 +1,21 @@
 import { F1 } from './data-source';
 
-
 export class DriversData extends F1 {
   constructor() {
     super();
   }
-   
-    async getDrivers() {
+
+  async getDrivers(pageElements: number = -1, page: number = 1) {
+    if (pageElements === -1) {
       return await this.get('drivers.json?limit=1000', {
-        cacheOptions: { ttl: 60}
+        cacheOptions: { ttl: 60 },
       });
+    }
+    const offset = (page - 1) * pageElements;
+    const limit = pageElements;
+    const filter = `limit=${limit}&offset=${offset}`;
+    return await this.get(`drivers.json?${filter}`, {
+      cacheOptions: { ttl: 60 },
+    });
   }
 }
